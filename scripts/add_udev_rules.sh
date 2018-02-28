5415bbafe37a5c16006b2a97ab75091335289aa0
splut <riordant@tcd.ie>
2018-02-28 19:47:15 +0000

    update script

--- scripts/add_udev_rules.sh
+++ scripts/add_udev_rules.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "SUBSYSTEMS==\"usb\", ATTRS{idVendor}==\"2c97\", ATTRS{idProduct}==\"0000\", MODE=\"0660\", TAG+=\"uaccess\", TAG+=\"udev-acl\", GROUP=\"plugdev\", OWNER=USER" >>/etc/udev/rules.d/20-hw1.rules
-echo "SUBSYSTEMS==\"usb\", ATTRS{idVendor}==\"2c97\", ATTRS{idProduct}==\"0001\", MODE=\"0660\", TAG+=\"uaccess\", TAG+=\"udev-acl\", GROUP=\"plugdev\", OWNER=USER" >>/etc/udev/rules.d/20-hw1.rules
+echo "SUBSYSTEMS==\"usb\", ATTRS{idVendor}==\"2c97\", ATTRS{idProduct}==\"0000\", MODE=\"0660\", TAG+=\"uaccess\", TAG+=\"udev-acl\", GROUP=\"plugdev\", OWNER=\"USER\"" >>/etc/udev/rules.d/20-hw1.rules
+echo "SUBSYSTEMS==\"usb\", ATTRS{idVendor}==\"2c97\", ATTRS{idProduct}==\"0001\", MODE=\"0660\", TAG+=\"uaccess\", TAG+=\"udev-acl\", GROUP=\"plugdev\", OWNER=\"USER\"" >>/etc/udev/rules.d/20-hw1.rules
 udevadm trigger
 udevadm control --reload-rules
